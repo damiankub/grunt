ef724661c32477f75f3c7ba7dff32d997f0c720a
DamianKub <damiankub89@gmail.com>
2017-09-19 09:19:27 +0200

    UpDate Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
             spawn: false,
       	  },
     	}
-	}
+	},
 
   	imagemin: {
   		dynamic: {
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
   				dest: 'images/build/'
   			}]
   		}
-  	}
+  	},
 
 
   });
